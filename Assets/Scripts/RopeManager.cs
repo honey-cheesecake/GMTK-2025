9edeed05415be0a7f7c44f4f087aec6c09ac298b
honey cheescake <83922479+honey-cheesecake@users.noreply.github.com>
2025-08-01 16:40:11 -0400

    Faster Ropes

--- Assets/Scripts/RopeManager.cs
+++ Assets/Scripts/RopeManager.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +8,21 @@ using UnityEngine.UIElements;
 public class RopeManager : MonoBehaviour
 {
     [SerializeField] float length;
-    [SerializeField][UnityEngine.Range(2, 100)] int numNodes;
-    [SerializeField][UnityEngine.Range(1, 10)] int constraintSteps;
+    [SerializeField][Range(2, 100)] int numNodes;
+    //[SerializeField][Range(0f, 1f)] float friction;
     [SerializeField] LineRenderer lineRenderer;
     [SerializeField] Camera cam;
     [SerializeField] CatManager catManager;
 
     Vector3[] positions;
+    Vector3[] prevPositions;
     InputAction mousePosAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         positions = new Vector3[numNodes];
+        prevPositions = new Vector3[numNodes];
         lineRenderer.positionCount = numNodes;
 
         mousePosAction = InputSystem.actions.FindAction("MousePos");
@@ -47,35 +48,37 @@ public class RopeManager : MonoBehaviour
         mousePos = cam.ScreenToWorldPoint(mousePos);
         mousePos.z = 0;
 
+        //for (int i = numNodes - 2; i >= 0; i--)
+        //{
+        //    // retain current velocity
+        //    Vector3 vel = positions[i] - prevPositions[i]; // velocity of current node
+        //    vel *= friction;
+        //    newPositions[i] = positions[i] + vel;
+        //}
+
         // Move the tip of the rope
         positions[numNodes - 1] = mousePos;
 
-        for (int i = 0; i < constraintSteps; i++)
+        // constraint
+        for (int idx = numNodes - 2; idx >= 0; idx--)
         {
-            for (int edgeIdx = 0; edgeIdx < numNodes - 1; edgeIdx++)
+            Vector3 nextPos = positions[idx + 1];
+            Vector3 currPos = positions[idx];
+
+            float targetLength = length / numNodes;
+            float actualLength = Vector2.Distance(nextPos, currPos);
+            if (actualLength > targetLength)
             {
-                int nextIdx = edgeIdx + 1;
-                int currIdx = edgeIdx;
-
-                Vector3 nextPos = positions[nextIdx];
-                Vector3 currPos = positions[currIdx];
-
-                float targetLength = length / numNodes;
-                float actualLength = Vector2.Distance(nextPos, currPos);
-                float lengthDelta = targetLength - actualLength;
-                Vector3 stickDir = (nextPos - currPos).normalized;
-
-                //if (currIdx != 0)
-                {
-                    positions[currIdx] -= stickDir * lengthDelta / 2;
-                }
-                if (nextIdx != numNodes - 1)
-                {
-                    positions[nextIdx] += stickDir * lengthDelta / 2;
-                }
+                Vector3 edgeDir = (currPos - nextPos).normalized;
+                positions[idx] = nextPos + edgeDir * targetLength;
             }
         }
 
+        // prepare for next frame
+        for (int i = 0; i < numNodes; i++)
+        {
+            prevPositions[i] = positions[i];
+        }
     }
 
     private void OnDrawGizmos()
